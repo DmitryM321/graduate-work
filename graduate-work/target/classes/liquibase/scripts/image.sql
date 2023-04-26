914b409790ab65d05822d2d6b85541c3de8afa68
dmitry <a89297175356@gmail.com>
2023-04-26 14:34:37 +0500

    graduate-work 07

--- graduate-work/target/classes/liquibase/scripts/image.sql
+++
@@ -1,9 +0,0 @@
--- liquibase formatted sql
-
--- changeset dmatveev:3
-create table image(
-    id         SERIAL PRIMARY KEY,
-    file_size  BIGINT,
-    media_type VARCHAR(255),
-    data       BYTEA
-);
